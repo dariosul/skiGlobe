f794b7f1517a1e064e8cfd2972b54e1fa2f02b9a
drusk <drusk@uvic.ca>
2016-06-07 19:56:36 -0700

    Added colours to globe for oceans, land and borders.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var planet = planetaryjs.planet();
 
 planet.loadPlugin(planetaryjs.plugins.earth({
-  topojson: { file: 'world-110m-withlakes.json' }
+  topojson: { file: 'world-110m-withlakes.json' },
+  oceans: { fill: '#000080' },
+  land: { fill: '#339966' },
+  borders: { fill: '#008000' }
 }));
 
 // Make the planet fit well in its canvas
